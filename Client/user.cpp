b90bf7a6ae2e623f67003af5a058e1be06729df4
Ruben <137336087+PaRubik163@users.noreply.github.com>
2025-02-06 13:29:37 +0300

    add server message

--- Client/user.cpp
+++ Client/user.cpp
@@ -1,5 +1,20 @@
 #include "user.h"
 
+#include <QJsonDocument>
+#include <QJsonObject>
+
+User::User()
+{
+    QObject::connect(this, &User::connected, [this]()
+    {
+        QJsonObject obj;
+        obj["name"] = name;
+        obj["type"] = "check name";
+        QJsonDocument doc(obj);
+        QByteArray bytes = doc.toJson();
+        write(bytes);
+    });
+}
 
 void User::connect(const QString &ip, quint16 port)
 {
